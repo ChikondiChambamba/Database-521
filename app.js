b9666ef1c968668836af64068c7c8e3cf344d457
WonganiEdgar01 <wonganiedgar@gmail.com>
2025-09-02 00:56:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 const pool = mysql.createPool({
   host: 'localhost',       
   user: 'root',            
-  password: 'root', 
+  password: '123456', 
   database: 'malawi_tourism',
   waitForConnections: true,
   connectionLimit: 10,
